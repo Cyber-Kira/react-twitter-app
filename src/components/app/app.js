89671f528999246b16eb4fd62eb180a07ac24b3f
Cyber-Kira <kirillkoloskovm@gmail.com>
2020-10-25 00:51:22 +0300

    added like and fav options

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import PostAddForm from '../post-add-form/';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        { label: "Going to learn React", important: true, id: 'fewf' },
+        { label: "That is so good", important: false, id: 'fasdf' },
+        { label: "I need a break...", important: false, id: 'ewg' },
+    ];
+
     return (
         <div className="app">
             <AppHeader />
@@ -16,7 +23,7 @@ const App = () => {
                 <SearchPanel />
                 <PostStatusFilter />
             </div>
-            <PostList />
+            <PostList posts = { data } />
             <PostAddForm />
         </div>
     );
