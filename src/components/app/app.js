8794bba295e190016ba8530bd3aa149afb798e62
Kirill <kkoloskov02@gmail.com>
2020-10-25 14:53:04 +0300

    added filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ export default class App extends React.Component {
             { label: "That is so good", important: false, liked: false, id: 2 },
             { label: "I need a break...", important: false, liked: false, id: 3 },
         ],
-        term: ''
+        term: '',
+        filter: 'all'
     }
 
     deleteItem = (id) => {
@@ -92,17 +93,29 @@ export default class App extends React.Component {
         return items.filter(item => item.label.indexOf(term) > -1)
     }
 
+    filterPosts = (items, filter) => {
+        if (filter === 'liked') {
+            return items.filter(item => item.liked);
+        } else {
+            return items;
+        }
+    }
+
     onUpdateSearch = (term) => {
         this.setState({term});
     }
 
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
         
         const liked = data.filter(item => item.liked).length;
         const allPosts = data.length;
 
-        const visiblePosts = this.searchPosts(data, term);
+        const visiblePosts = this.filterPosts(this.searchPosts(data, term), filter);
 
         return (
             <div className="app">
@@ -114,7 +127,10 @@ export default class App extends React.Component {
                     <SearchPanel 
                         onUpdateSearch={this.onUpdateSearch}
                     />
-                    <PostStatusFilter />
+                    <PostStatusFilter 
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect}
+                    />
                 </div>
                 <PostList 
                     posts = { visiblePosts }
